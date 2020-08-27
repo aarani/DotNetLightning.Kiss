6bb46e0189f337958d61e55320c9eb82f3031c1f
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-08-27 11:09:35 +0200

    Prevent possible F#4.0 issue
    It seems that these attributes can cause this compilation error in F#4.0:
    error FS0927: The kind of the type specified by its attributes does not match the kind implied by its definition
    
    Newer versions of F# (like 4.5 or even newer, like the one being used
    by .NETCore to build the binary that is later published in nuget) allow
    compiling this code with no issues, but if you reference the generated
    assembly later from an old F# compiler, it could generate exceptions at
    runtime, e.g.: System.BadFormatImageException (or other types) whose
    inner exception could be the following:
    
    System.TypeLoadException : Could not load type of field 'GWallet.Backend.UtxoCoin.Lightning.SerializedChannel:MinSafeDepth@' (6) due to: Expected reference type but got type kind 17
    
    FSharp.Core's Result type is also affected by this so in this commit we
    create a replacement for it that is only used in the BouncyCastle build
    (which we now rename as 'Portability' build).
    
    Coauthored-by: Andres G. Aragoneses <knocte@gmail.com>

--- src/DotNetLightning.Core/Utils/Primitives.fs
+++ src/DotNetLightning.Core/Utils/Primitives.fs
@@ -10,6 +10,7 @@ open System.Linq
 open System.Diagnostics
 open DotNetLightning.Core.Utils.Extensions
 open ResultUtils
+open ResultUtils.Portability
 
 [<AutoOpen>]
 module Primitives =
@@ -32,7 +33,9 @@ module Primitives =
             output
 
     /// Absolute block height
+#if !NoDUsAsStructs
     [<Struct>]
+#endif
     type BlockHeight = | BlockHeight of uint32 with
         static member Zero = 0u |> BlockHeight
         static member One = 1u |> BlockHeight
@@ -58,7 +61,11 @@ module Primitives =
     /// 16bit relative block height used for `OP_CSV` locks,
     /// Since OP_CSV allow only block number of 0 ~ 65535, it is safe
     /// to restrict into the range smaller than BlockHeight
-    and  [<Struct>] BlockHeightOffset16 = | BlockHeightOffset16 of uint16 with
+    and
+#if !NoDUsAsStructs
+        [<Struct>]
+#endif
+        BlockHeightOffset16 = | BlockHeightOffset16 of uint16 with
         member x.Value = let (BlockHeightOffset16 v) = x in v
 
         static member ofBlockHeightOffset32(bho32: BlockHeightOffset32) =
@@ -77,7 +84,11 @@ module Primitives =
     ///
     /// 32bit relative block height. For `OP_CSV` locks, BlockHeightOffset16
     /// should be used instead.
-    and  [<Struct>] BlockHeightOffset32 = | BlockHeightOffset32 of uint32 with
+    and
+#if !NoDUsAsStructs
+        [<Struct>]
+#endif
+        BlockHeightOffset32 = | BlockHeightOffset32 of uint32 with
         member x.Value = let (BlockHeightOffset32 v) = x in v
 
         static member ofBlockHeightOffset16(bho16: BlockHeightOffset16) =
@@ -336,23 +347,32 @@ module Primitives =
     type BlockId = | BlockId of uint256 with
         member x.Value = let (BlockId v) = x in v
 
+#if !NoDUsAsStructs
     [<Struct>]
+#endif
     type HTLCId = | HTLCId of uint64 with
         static member Zero = HTLCId(0UL)
         member x.Value = let (HTLCId v) = x in v
 
         static member (+) (a: HTLCId, b: uint64) = (a.Value + b) |> HTLCId
 
+#if !NoDUsAsStructs
     [<Struct>]
+#endif
     type TxOutIndex = | TxOutIndex of uint16 with
         member x.Value = let (TxOutIndex v) = x in v
 
+#if !NoDUsAsStructs
     [<Struct>]
+#endif
     type TxIndexInBlock = | TxIndexInBlock of uint32 with
         member x.Value = let (TxIndexInBlock v) = x in v
 
 
-    [<Struct;StructuredFormatDisplay("{AsString}")>]
+#if !NoDUsAsStructs
+    [<Struct>]
+#endif
+    [<StructuredFormatDisplay("{AsString}")>]
     type ShortChannelId = {
         BlockHeight: BlockHeight
         BlockIndex: TxIndexInBlock
@@ -413,8 +433,10 @@ module Primitives =
     type EncodingType =
         | SortedPlain = 0uy
         | ZLib = 1uy
-    
-    [<StructAttribute>]
+
+#if !NoDUsAsStructs
+    [<Struct>]
+#endif
     type CommitmentNumber(index: UInt48) =
         member this.Index = index
 
@@ -473,7 +495,11 @@ module Primitives =
                     remotePaymentBasePoint
             ObscuredCommitmentNumber((UInt48.MaxValue - this.Index) ^^^ obscureFactor)
 
-    and [<StructAttribute>] ObscuredCommitmentNumber(obscuredIndex: UInt48) =
+    and
+#if !NoDUsAsStructs
+        [<Struct>]
+#endif
+        ObscuredCommitmentNumber(obscuredIndex: UInt48) =
         member this.ObscuredIndex: UInt48 = obscuredIndex
 
         override this.ToString() =
@@ -513,7 +539,9 @@ module Primitives =
                     remotePaymentBasePoint
             CommitmentNumber(UInt48.MaxValue - (this.ObscuredIndex ^^^ obscureFactor))
 
-    [<StructAttribute>]
+#if !NoDUsAsStructs
+    [<Struct>]
+#endif
     type RevocationKey(key: Key) =
         member this.Key = key
 
@@ -545,7 +573,11 @@ module Primitives =
         member this.CommitmentPubKey: CommitmentPubKey =
             CommitmentPubKey this.Key.PubKey
 
-    and [<StructAttribute>] CommitmentPubKey(pubKey: PubKey) =
+    and
+#if !NoDUsAsStructs
+        [<Struct>]
+#endif
+        CommitmentPubKey(pubKey: PubKey) =
         member this.PubKey = pubKey
 
         static member BytesLength: int = PubKey.BytesLength
@@ -557,7 +589,9 @@ module Primitives =
             this.PubKey.ToBytes()
 
 
-    [<StructAttribute>]
+#if !NoDUsAsStructs
+    [<Struct>]
+#endif
     type CommitmentSeed(masterRevocationKey: RevocationKey) =
         new(key: Key) =
             CommitmentSeed(RevocationKey key)
